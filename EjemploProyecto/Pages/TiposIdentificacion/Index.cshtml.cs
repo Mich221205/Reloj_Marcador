e9b5b1647e500b31863f9af6a486136a06d5ed84
Jocs <fonsecaajocs@gmail.com>
2025-10-07 11:28:35 -0600

    TIPOIDENTIFICACION

--- EjemploProyecto/Pages/TiposIdentificacion/Index.cshtml.cs
+++ EjemploProyecto/Pages/TiposIdentificacion/Index.cshtml.cs
@@ -1,12 +1,36 @@
-using Microsoft.AspNetCore.Mvc;
+using EjemploCoreWeb.Entities;
+using EjemploCoreWeb.Services.Abstract;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
-namespace EjemploProyecto.Pages.TiposIdentificacion
+namespace EjemploProyecto.Pages.ADM_Identificacion
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ITipoIdentificacionService _service;
+        private readonly IBitacoraService _bitacoraService;
+
+        public IndexModel(ITipoIdentificacionService service, IBitacoraService bitacoraService)
         {
+            _service = service;
+            _bitacoraService = bitacoraService;
+        }
+
+        public IEnumerable<TipoIdentificacion> Tipos { get; set; }
+
+        public async Task OnGetAsync()
+        {
+
+            Tipos = _service.ObtenerTodos();
+
+
+            int idUsuario = 1; //Temporal — luego se reemplaza con el usuario logueado
+            await _bitacoraService.Registrar(
+                idUsuario,
+                idAccion: 4,
+                detalle: null,
+                nombreAccion: "El usuario consulta Tipos de Identificación"
+            );
         }
     }
 }
