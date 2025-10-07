5c3f182878ba283be915a4ec5562c1d00e9d7910
Jocs <fonsecaajocs@gmail.com>
2025-10-07 10:56:31 -0600

    Update RolService.cs

--- EjemploCoreWeb.Services/RolService.cs
+++ EjemploCoreWeb.Services/RolService.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using EjemploCoreWeb.Entities;
+using EjemploCoreWeb.Repository;
+using EjemploCoreWeb.Services.Abstract;
 
 namespace EjemploCoreWeb.Services
 {
-    internal class RolService
+    public class RolService : IRolService
     {
+        private readonly RolRepository _rolRepository;
+
+        public RolService(RolRepository rolRepository)
+        {
+            _rolRepository = rolRepository;
+        }
+
+        public IEnumerable<Rol> ObtenerRoles() => _rolRepository.GetAll();
+
+        public void CrearRol(Rol rol) => _rolRepository.Insert(rol);
+
+        public void ActualizarRol(Rol rol) => _rolRepository.Update(rol);
+
+        public void EliminarRol(int idRol) => _rolRepository.Delete(idRol);
     }
 }
