6360666b7f720e41bdb1e1b8b22bd48e1c4d47a2
Jocs <fonsecaajocs@gmail.com>
2025-10-07 11:39:14 -0600

    Update Area.cs

--- EjemploCoreWeb.Entities/Area.cs
+++ EjemploCoreWeb.Entities/Area.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EjemploCoreWeb.Entities
+namespace EjemploCoreWeb.Entities;
+
+public class Area
 {
-    internal class Area
-    {
-    }
+    public int ID_Area { get; set; }
+    public string Nombre_Area { get; set; } = "";
+    public int Jefe_Area { get; set; }
+    public string Codigo_Area { get; set; } = "";
+
+    public string? Jefe_Nombre { get; set; }
 }
