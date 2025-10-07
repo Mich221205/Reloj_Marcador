c13cf6d3ea33d5607237562079a587b138b16ed5
Jocs <fonsecaajocs@gmail.com>
2025-10-07 11:05:29 -0600

    agregando montse

---
+++ EjemploProyecto/Pages/TiposIdentificacion/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EjemploProyecto.Pages.TiposIdentificacion
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
