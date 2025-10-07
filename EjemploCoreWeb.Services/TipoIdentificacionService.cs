05dc08943109e20b193155aff43b3f9b40d18225
Jocs <fonsecaajocs@gmail.com>
2025-10-07 10:54:57 -0600

    Update TipoIdentificacionService.cs

--- EjemploCoreWeb.Services/TipoIdentificacionService.cs
+++ EjemploCoreWeb.Services/TipoIdentificacionService.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using EjemploCoreWeb.Entities;
+using EjemploCoreWeb.Repository;
+using EjemploCoreWeb.Services.Abstract;
 
 namespace EjemploCoreWeb.Services
 {
-    internal class TipoIdentificacionService
+    public class TipoIdentificacionService : ITipoIdentificacionService
     {
+        private readonly IdentificacionRepository _repo;
+
+        public TipoIdentificacionService(IdentificacionRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public IEnumerable<TipoIdentificacion> ObtenerTodos() => _repo.GetAll();
+        public void Crear(TipoIdentificacion tipo) => _repo.Insert(tipo);
+        public void Actualizar(TipoIdentificacion tipo) => _repo.Update(tipo);
+        public void Eliminar(int idTipo) => _repo.Delete(idTipo);
     }
 }
+
