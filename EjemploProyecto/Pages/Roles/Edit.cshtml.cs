3b2e8417594e1aca4b65b833319be4aa4825c1d8
Jocs <fonsecaajocs@gmail.com>
2025-10-07 11:16:59 -0600

    gfdgfd

--- EjemploProyecto/Pages/Roles/Edit.cshtml.cs
+++ EjemploProyecto/Pages/Roles/Edit.cshtml.cs
@@ -1,12 +1,70 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using EjemploCoreWeb.Entities;
+using EjemploCoreWeb.Services;
+using System.Linq;
+using EjemploCoreWeb.Services.Abstract;
 
 namespace EjemploProyecto.Pages.Roles
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly IRolService _rolService;
+        private readonly IBitacoraService _bitacoraService;
+
+        [BindProperty]
+        public Rol Rol { get; set; }
+
+
+        public EditModel(IRolService rolService, IBitacoraService bitacoraService)
         {
+            _rolService = rolService;
+            _bitacoraService = bitacoraService;
+        }
+
+        public void OnGet(int id)
+        {
+            Rol = _rolService.ObtenerRoles().FirstOrDefault(r => r.ID_Rol_Usuario == id);
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                var errores = string.Join("<br>",
+                    ModelState.Values.SelectMany(v => v.Errors)
+                                     .Select(e => e.ErrorMessage));
+
+                ViewData["ModalType"] = "error";
+                ViewData["ModalTitle"] = "Error de validación";
+                ViewData["ModalMessage"] = errores;
+
+                return Page();
+            }
+
+            try
+            {
+                var anterior = _rolService.ObtenerRoles()
+                    .FirstOrDefault(r => r.ID_Rol_Usuario == Rol.ID_Rol_Usuario);
+
+                _rolService.ActualizarRol(Rol);
+
+
+                await _bitacoraService.Registrar(1, 2, new { Antes = anterior, Despues = Rol }, "UPDATE");
+
+                ViewData["ModalType"] = "success";
+                ViewData["ModalTitle"] = "Actualización exitosa";
+                ViewData["ModalMessage"] = "El rol fue actualizado correctamente.";
+                ViewData["RedirectPage"] = "Index";
+            }
+            catch (InvalidOperationException ex)
+            {
+                ViewData["ModalType"] = "error";
+                ViewData["ModalTitle"] = "Error";
+                ViewData["ModalMessage"] = ex.Message;
+            }
+
+            return Page();
         }
     }
 }
